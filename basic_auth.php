1c10bab8a153c2f516aecce9f4c26786547e9477
mavlyukaev <113362672+mavlyukaev@users.noreply.github.com>
2023-05-23 20:23:46 +0300

    Create basic_auth.php

---
+++ basic_auth.php
@@ -0,0 +1,32 @@
+<?php
+
+if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
+    requireLogin();
+}
+
+$user = 'u52887';
+$pass = '1703150';
+$db = new PDO('mysql:host=localhost;dbname=u52887', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+$stmt = $db->prepare("SELECT login, password FROM admins WHERE login = ?");
+$stmt->execute([$_SERVER['PHP_AUTH_USER']]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($row) {
+    $validUser = $row['login'];
+    $validPassHash = $row['password'];
+} else {
+    requireLogin();
+}
+
+if ($_SERVER['PHP_AUTH_USER'] != $validUser || md5($_SERVER['PHP_AUTH_PW']) != $validPassHash) {
+    requireLogin();
+}
+session_start();
+
+function requireLogin() {
+    header('HTTP/1.1 401 Unanthorized');
+    header('WWW-Authenticate: Basic realm="My site"');
+    print('<h1>401 Требуется авторизация</h1>');
+    exit();
+}
